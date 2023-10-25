de309a7267e786e4aa6c936b80321adf6579a255
Yasmin Afsharinejad <iasaminblack@gmail.com>
2023-10-25 18:37:23 -0300

    Updated Portfolio

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
 import '@/styles/globals.css'
+import Head from "next/head";
+import { Montserrat } from "next/font/google"
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont"
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="https://res.cloudinary.com/dsrdpgpzy/image/upload/v1698188265/Portfolio/favicon_bfwbab.ico"/>
+      </Head>
+      <main className={`${montserrat.variable} font-mont`}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,8 +9,6 @@ export default function Home() {
       <Head>
         <title>My Portfolio - Yasmin Afsharinejad</title>
         <meta name="description" content="I have created this beautiful portfolio so that you can access all the work and projects carried out in what I like most, which is programming"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="https://res.cloudinary.com/dsrdpgpzy/image/upload/v1698188265/Portfolio/favicon_bfwbab.ico"/>
       </Head>
       <main>
         Home
