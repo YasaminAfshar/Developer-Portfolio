a7964c3788ce48b9e5b58600df99d5beefb8490a
Yasmin Afsharinejad <iasaminblack@gmail.com>
2024-01-02 15:50:23 -0300

    Updated Portfolio

--- src/subComponents/Logo.js
+++ src/subComponents/Logo.js
@@ -31,6 +31,8 @@ const Logo = () => {
       >
         <img
           className="w-8 1md:w-11"
+          width={8}
+          height={8}
           src="/images/svgs/power-off.svg"
           alt="Button with direct access to Home Page"
         />
